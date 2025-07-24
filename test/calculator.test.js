bc7a46acf92ac9fa4be9df7ddcf2b80bf4e31c93
Deibson Cogo <deibsoncogo@outlook.com>
2025-07-23 21:12:56 -0300

    test: criação do primeiro teste de exemplo

---
+++ test/calculator.test.js
@@ -0,0 +1,14 @@
+import { expect } from "chai"
+import { sum } from "../src/calculator.js"
+
+describe("Sum function", () => {
+  it("Must return the sum of two numbers", () => {
+    const result = sum(2, 3)
+    expect(result).to.equal(5)
+  })
+
+  it("Must return the correct sum with negative numbers", () => {
+    const result = sum(-1, -1)
+    expect(result).to.equal(-2)
+  })
+})
