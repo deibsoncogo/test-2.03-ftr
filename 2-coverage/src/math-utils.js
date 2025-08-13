23da5425136e7052d1f2d9c7d3b5d39090647387
Deibson Cogo <deibsoncogo@outlook.com>
2025-08-13 11:17:11 -0300

    test: criação de novos testes com visualização diferente da cobertura

--- 2-coverage/src/math-utils.js
+++ 2-coverage/src/math-utils.js
@@ -5,3 +5,8 @@ export function sum(a, b) {
 
   return a + b
 }
+
+export function divide(a, b) {
+  if (b === 0) throw new Error("Division by zero")
+  return a / b
+}
--- 2-coverage/test/math-utils.test.js
+++ 2-coverage/test/math-utils.test.js
@@ -1,5 +1,5 @@
 import { expect } from "chai"
-import { sum } from "../src/math-utils.js"
+import { divide, sum } from "../src/math-utils.js"
 
 describe("Sum", () => {
   it("Should add two numbers", () => {
@@ -8,5 +8,15 @@ describe("Sum", () => {
 
   it("Should throw error for non-number inputs", () => {
     expect(() => sum("2", 3)).to.throw("Invalid arguments")
+  })
+})
+
+describe("Divide", () => {
+  it("Should divide two numbers", () => {
+    expect(divide(6, 3)).to.equal(2)
+  })
+
+  it("Should throw error for division by zero", () => {
+    expect(() => divide(6, 0)).to.throw("Division by zero")
   })
 })
